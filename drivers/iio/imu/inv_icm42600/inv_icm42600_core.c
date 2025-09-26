db036664335d1fd97cda90c50fc0d9b45e6e7f3f
Koki Shinjo <kshinjo@pfrobotics.jp>
2025-09-26 17:23:41 +0900

    Fix driver
    Change-Id: I1a1bf25c738c9afb68da5e692e526f16c244e54a

--- drivers/iio/imu/inv_icm42600/inv_icm42600_core.c
+++ drivers/iio/imu/inv_icm42600/inv_icm42600_core.c
@@ -767,8 +767,7 @@ out_unlock:
 static int inv_icm42600_resume(struct device *dev)
 {
 	struct inv_icm42600_state *st = dev_get_drvdata(dev);
-	struct inv_sensors_timestamp *gyro_ts = iio_priv(st->indio_gyro);
-	struct inv_sensors_timestamp *accel_ts = iio_priv(st->indio_accel);
+	struct inv_sensors_timestamp *dev_ts = iio_priv(st->indio_dev);
 	int ret;
 
 	mutex_lock(&st->lock);
@@ -790,8 +789,7 @@ static int inv_icm42600_resume(struct device *dev)
 
 	/* restore FIFO data streaming */
 	if (st->fifo.on) {
-		inv_sensors_timestamp_reset(gyro_ts);
-		inv_sensors_timestamp_reset(accel_ts);
+		inv_sensors_timestamp_reset(dev_ts);
 		ret = regmap_write(st->map, INV_ICM42600_REG_FIFO_CONFIG,
 				   INV_ICM42600_FIFO_CONFIG_STREAM);
 	}
